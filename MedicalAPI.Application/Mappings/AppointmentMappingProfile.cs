ad03e05569dff947d7bbaa48ed9d7c03018b5ed9
Kacper <173662@o365.prz.edu.pl>
2024-11-30 17:05:40 +0100

    nic nowego dodanie automappera, oraz wyświetlania pacjentów w widoku

---
+++ MedicalAPI.Application/Mappings/AppointmentMappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MedicalAPI.Application.Appointment;
+using MedicalAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalAPI.Application.Mappings
+{
+    public class AppointmentMappingProfile : Profile
+    {
+        public AppointmentMappingProfile()
+        {
+            CreateMap<AppointmentDto, Domain.Entities.Appointment>()
+                .ForMember(e => e.Patient, opt => opt.MapFrom(src => new Patient()
+                {
+                    AppointmentId = src.AppointmentId,
+                    PatientId = src.PatientId
+                }));
+           //CreateMap<DoctorDto>     todo: dodać potem mapping 
+        }
+    }
+}
